a6797fb2a1d79a76dc5749ad0b108ccb82410c0e
KacperC948 <miloord899@gmail.com>
2023-12-19 21:27:06 +0100

    Added swagger to project and update security configuration

--- backend-api/culinaryBlogApi/src/main/java/com/example/culinaryblogapi/config/SecurityConfiguration.java
+++ backend-api/culinaryBlogApi/src/main/java/com/example/culinaryblogapi/config/SecurityConfiguration.java
@@ -22,12 +22,24 @@ public class SecurityConfiguration {
 
     private final AuthenticationProvider authenticationProvider;
 
+    private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
-                        req.requestMatchers("/api/v1/auth/login")
+                        req.requestMatchers(WHITE_LIST_URL)
                                 .permitAll()
                                 .requestMatchers("/api/v1/auth/register").hasAuthority("ADMIN")
                                 .anyRequest()
